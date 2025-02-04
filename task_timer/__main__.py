c53a7079260ba11d7b9d9ec8fc1d18edde2f5b62
Abe Gomez <abraham.gomez@cune.org>
2025-02-04 12:15:34 -0600

    Menu/Button Creation: Create, Delete, and Exit

--- task_timer/__main__.py
+++ task_timer/__main__.py
@@ -1,10 +1,60 @@
 import click
 
 @click.command()
+def menu():
+
+    click.echo("\nCHOICES:")
+    click.echo("---------")
+    click.echo("\n(C)........Create a task")
+    click.echo("\n(D)........Delete a task")
+    click.echo("\n(E)........Exit")
+    click.echo("\nInput the indicated letter for the option you wish to select: ")
+
+    choice = input()
+
+    if choice == 'C':
+        create_task()
+
+    if choice == 'D':
+        delete_task()
+
+    if choice == 'E':
+        exit()
+
+@click.command()
+def create_task():
+
+    create = input("\nCreate a task: ")
+    tasks.append(create)
+
+    click.echo("\nTASKS:")
+    click.echo("-------")
+
+    for i, item in enumerate(tasks, start=1):
+        click.echo(f"{i}. {item}")
+
+    return menu()
+
+@click.command()
+def delete_task():
+
+    index = int(input()) - 1
+    tasks.pop(index)
+
+    for i, item in enumerate(tasks, start=1):
+        click.echo(f"{i}. {item}")
+
+
+
+
+tasks = []
+
+@click.command()
 def main():
     """This is my main cli."""
 
-    click.echo("Hello World")
+    menu()
+    
 
 if __name__ == '__main__':
     main()
