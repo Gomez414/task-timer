8e57e5587a7af729e49ef3ace162e0798a796187
Abe Gomez <abraham.gomez@cune.org>
2025-02-05 20:07:02 -0600

    First Draft Ideas

--- task_timer/__main__.py
+++ task_timer/__main__.py
@@ -1,13 +1,18 @@
 import click
+import csv
+import time
 
 @click.command()
 def menu():
 
     click.echo("\nCHOICES:")
     click.echo("---------")
-    click.echo("\n(C)........Create a task")
-    click.echo("\n(D)........Delete a task")
-    click.echo("\n(E)........Exit")
+    click.echo("\n(C)........Create a task.")
+    click.echo("\n(D)........Delete a task.")
+    click.echo("\n(S)........Start timing a task.")
+    click.echo("\n(P)........Stop timing a task.")
+    click.echo("\n(E)........Exit.")
+    click.echo("\n(F)........Export timesheet as CVS file.")
     click.echo("\nInput the indicated letter for the option you wish to select: ")
 
     choice = input()
@@ -18,6 +23,15 @@ def menu():
     if choice == 'D':
         delete_task()
 
+    if choice == 'S':
+        start_timer()
+
+    if choice == 'P':
+        stop_timer()
+
+    if choice == "F":
+        export_as_cvs_file()
+
     if choice == 'E':
         exit()
 
@@ -26,6 +40,7 @@ def create_task():
 
     create = input("\nCreate a task: ")
     tasks.append(create)
+
 
     click.echo("\nTASKS:")
     click.echo("-------")
@@ -44,6 +59,33 @@ def delete_task():
     for i, item in enumerate(tasks, start=1):
         click.echo(f"{i}. {item}")
 
+@click.command()
+def start_timer():
+    task_choice = input("/nEnter the name of the task you wish to start: ")
+
+    tasks[task_choice] = time.time()
+
+    click.echo(f"{task_choice} started at {tasks}")
+    
+
+def stop_timer():
+
+
+
+def export_as_cvs_file():
+
+    with open('timesheet.csv', 'w', newline='') as csvfile:
+
+        fieldnames = ['task_name', 'start_time', 'status']
+
+        thewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        thewriter.writeheader()
+
+        for task in tasks:
+            thewriter.writerow({'task_name': task })
+
+    return menu()
 
 
 
@@ -52,7 +94,7 @@ tasks = []
 @click.command()
 def main():
     """This is my main cli."""
-
+        
     menu()
     
 
